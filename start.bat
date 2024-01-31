21875ed6591db7249efb55a1dfe29c9aa4bd124d
leonsillano <leonsillano@yahoo.fr>
2024-01-31 16:12:17 +0700

    Changing start.bat

--- start.bat
+++ start.bat
@@ -1,13 +1,5 @@
-@echo off
-
-REM Exécute npm install
 npm install
 
-REM Ouvre la page web dans Firefox
-start "" "http://localhost:8000"
-
-REM Exécute npm start en premier plan
 node index.js
 
-REM Attend que l'utilisateur appuie sur "Entrée" pour quitter
 set /p input="Press enter to quit..."
