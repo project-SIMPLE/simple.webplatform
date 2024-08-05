33da90e8941bc3ad783263065c25fedfeb4321ea
RoiArthurB <contact@arthurbrugiere.fr>
2024-08-05 23:41:48 +0700

    feat: Refactoring monitor_server file

--- src/monitor_server.js
+++ src/monitor_server.js
@@ -1,7 +1,6 @@
 //Imports
 const WebSocket = require('ws');
-
-var monitor_socket_clients = []
+const {useVerbose} = require("../index");
 
 /**
  * Creates a Websocket Server for handling monitor connections
@@ -14,53 +13,82 @@ class MonitorServer {
     constructor(controller) {
         this.controller = controller;
         const monitor_server = this
-        this.monitor_ws_port = process.env.MONITOR_WS_PORT;
-        var monitor_ws_port = this.monitor_ws_port
-        this.monitor_socket = new WebSocket.Server({ port: this.monitor_ws_port });
+        this.monitor_socket = new WebSocket.Server({ port: process.env.MONITOR_WS_PORT });
 
-        this.monitor_socket.on('error', (err) => {
-            if (err.code === 'EADDRINUSE') {
-                console.log(`\x1b[31m-> The port ${monitor_ws_port} is already in use. Choose a different port in settings.json.\x1b[0m`);
-            }
-            else {
-                console.log(`\x1b[31m-> An error occured for the monitor server, code: ${err.code}\x1b[0m`)
-            }
-        })
+        this.monitor_socket_clients = [];
 
-        this.monitor_socket.on('connection', function connection(ws) {
-            monitor_socket_clients.push(ws)
-            monitor_server.sendMonitorJsonState();
-            monitor_server.sendMonitorJsonSettings()
-            ws.on('message', function incoming(message) {
+        /*
+            Handling middleware socket connections and message routing
+         */
+
+        this.monitor_socket.on('connection', (socket) => {
+            this.monitor_socket_clients.push(socket)
+            this.sendMonitorJsonState();
+            this.sendMonitorJsonSettings();
+            socket.on('message', (message) => {
                 try {
                     const json_monitor = JSON.parse(message)
                     const type = json_monitor['type']
-                    if (type == "launch_experiment") controller.launchExperiment()
-                    else if (type == "stop_experiment") controller.stopExperiment()
-                    else if (type == "pause_experiment") controller.pauseExperiment()
-                    else if (type == "resume_experiment") controller.resumeExperiment()
-                    else if (type == "try_connection") controller.connectGama()
-                    else if (type == "add_every_players") controller.addInGameEveryPlayers()
-                    else if (type == "remove_every_players") controller.removeInGameEveryPlayers()
-                    else if (type == "add_player_headset") controller.addInGamePlayer(json_monitor["id"])
-                    else if (type == "remove_player_headset") controller.removeInGamePlayer(json_monitor["id"])
-                    else if (type == "json_settings") controller.changeJsonSettings(json_monitor)
-                    else if (type == "clean_all") controller.cleanAll()
-                    else console.log("\x1b[31m-> The last message received from the monitor had an unknown type.\x1b[0m");
+                    switch (type){
+                        case "launch_experiment":
+                            this.controller.launchExperiment();
+                            break;
+                        case "stop_experiment":
+                            this.controller.stopExperiment();
+                            break;
+                        case "pause_experiment":
+                            this.controller.pauseExperiment();
+                            break;
+                        case "resume_experiment":
+                            this.controller.resumeExperiment();
+                            break;
+                        case "try_connection":
+                            this.controller.connectGama();
+                            break;
+                        case "add_every_players":
+                            this.controller.addInGameEveryPlayers();
+                            break;
+                        case "remove_every_players":
+                            this.controller.removeInGameEveryPlayers();
+                            break;
+                        case "add_player_headset":
+                            this.controller.addInGamePlayer(json_monitor["id"]);
+                            break;
+                        case "remove_player_headset":
+                            this.controller.removeInGamePlayer(json_monitor["id"]);
+                            break;
+                        case "json_settings":
+                            this.controller.changeJsonSettings(json_monitor);
+                            break;
+                        case "clean_all":
+                            this.controller.cleanAll();
+                            break;
+                        default:
+                            console.log("\x1b[31m-> The last message received from the monitor had an unknown type.\x1b[0m");
+                    }
                 }
                 catch (exception) {
                     console.error("\x1b[31m-> The last message received from the monitor created an internal error.\x1b[0m");
                     console.error(exception);
                 }
-            })
+            });
         });
+
+        this.monitor_socket.on('error', (err) => {
+            if (err.code === 'EADDRINUSE') {
+                console.log(`\x1b[31m-> The port ${process.env.MONITOR_WS_PORT} is already in use. Choose a different port in settings.json.\x1b[0m`);
+            }
+            else {
+                console.log(`\x1b[31m-> An error occured for the monitor server, code: ${err.code}\x1b[0m`)
+            }
+        })
     }
 
     /**
      * Sends the json_state to the monitor
      */
     sendMonitorJsonState() {
-        if (monitor_socket_clients != undefined) monitor_socket_clients.forEach((client) => {
+        if (this.monitor_socket_clients !== undefined) this.monitor_socket_clients.forEach((client) => {
             client.send(JSON.stringify(this.controller.modelManager.getModelList()[this.controller.choosedLearningPackageIndex].getAll()));
         })
     }
@@ -69,7 +97,7 @@ class MonitorServer {
      * Send the json_setting to the monitor
      */
     sendMonitorJsonSettings() {
-        if (monitor_socket_clients != undefined) monitor_socket_clients.forEach((client) => {
+        if (this.monitor_socket_clients !== undefined) this.monitor_socket_clients.forEach((client) => {
             client.send(JSON.stringify(this.controller.modelManager.getModelList()[this.controller.choosedLearningPackageIndex].getJsonSettings()));
         })
     }
