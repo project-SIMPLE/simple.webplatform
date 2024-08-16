6082538ebbca472c6729606b4f6dd5dfc66733d1
Tuan <tuan-truong@live.fr>
2024-08-16 10:02:44 +0700

    feat : display error message if no simulation is selected

--- src/components/MainPanel/MainPanel.tsx
+++ src/components/MainPanel/MainPanel.tsx
@@ -68,6 +68,7 @@ const MainPanel: React.FC = () => {
     }
   };
 
+  // change & display state of the simulation
   const renderExperimentState = () => {
     switch (gama.experiment_state) {
       case 'RUNNING':
@@ -137,161 +138,173 @@ const MainPanel: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <div className="w-2/3 bg-white p-8 shadow-lg rounded-lg text-center">
+      {/* case Simulation Selected */}
       {selectedSimulation ? (
-      <div className="text-3xl mb-4">Waiting {selectedSimulation.name} .. </div> 
+      <div> 
+        <div className="text-3xl mb-4">Waiting {selectedSimulation.name} .. </div> 
+        <div className="flex justify-center mb-4">
+            <svg
+              className={`w-6 h-6 mr-2 ${gama.connected ? 'text-green-500' : 'text-gray-500'}`}
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 13l4 4L19 7"
+              />
+            </svg>
+            <span className={gama.connected ? 'text-green-500' : 'text-gray-500'}>
+              {gama.connected ? 'Connected' : 'Waiting for connection'}
+            </span>
+          </div>
+        
+          {/*should create a component for this function*/}
+          {renderExperimentState()}
+        
+          {/* Display Buttons to monitor the simulation */}
+          <div className="flex justify-center mb-4">
+            <Button onClick={handleTryConnection} text="Try Connection" bgColor="bg-gray-500" icon={
+              <svg
+                className="w-6 h-6 mr-2"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M14.752 11.168l-6.5-3.75A1 1 0 007 8.25v7.5a1 1 0 001.252.832l6.5-3.75a1 1 0 000-1.664z"
+                />
+              </svg>
+            } showText={true} />
+          </div>
+
+          <div className="flex justify-center space-x-2">
+            <Button onClick={handleLoad} text="Load" bgColor="bg-blue-500" icon={
+              <svg
+                className="w-6 h-6 mr-2"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M12 4v16m8-8H4"
+                />
+              </svg>
+            } showText={true} />
+            <Button onClick={handlePlayPause} text="Play/Pause" bgColor="bg-green-500" icon={
+              <svg
+                className="w-6 h-6 mr-2"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M10 9v6m4-6v6"
+                />
+              </svg>
+            } showText={true} />
+            <Button onClick={handleEnd} text="End" bgColor="bg-red-500" icon={
+              <svg
+                className="w-6 h-6 mr-2"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            } showText={true} />
+          </div>
+          
+          {/* Display List of Players */}
+          <div className="flex justify-center mt-8 space-x-4">
+            
+            {Object.keys(playerList).map((key, index) => {
+              const player = playerList[key];
+              return (
+                <div key={index} className="flex flex-col items-center">
+                  <VRHeadset isConnected={player.connected} />
+                  <p>{player.connected ? 'Connected' : 'Waiting for connection..'}</p>
+                  
+                  {/* Buttons under each players */}
+                  <div className="flex mt-4 space-x-2">
+                    <Button
+                      onClick={() => handleRemove(index)}
+                      text="Remove"
+                      bgColor="bg-red-500"
+                      icon={
+                        <svg
+                          className="w-6 h-6 mr-2"
+                          fill="none"
+                          stroke="currentColor"
+                          viewBox="0 0 24 24"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M6 18L18 6M6 6l12 12"
+                          />
+                        </svg>
+                      }
+                      showText={false}
+                    />
+                    <Button
+                      onClick={() => handleRestart(index)}
+                      text="Restart"
+                      bgColor="bg-orange-500"
+                      icon={
+                        <svg
+                          className="w-6 h-6 mr-2"
+                          fill="none"
+                          stroke="currentColor"
+                          viewBox="0 0 24 24"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M4 4v6h6M20 20v-6h-6M4 10c1.5-2 4-3 6-3h4c2 0 4 1 5 3M4 14c1.5 2 4 3 6 3h4c2 0 4-1 5-3"
+                          />
+                        </svg>
+                      }
+                      showText={false}
+                    />
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+      </div>
+
       ) : (
+          // case No Simulation Selected
           <div className="text-3xl mb-4">No simulation selected</div> 
       )}
-      <div className="flex justify-center mb-4">
-          <svg
-            className={`w-6 h-6 mr-2 ${gama.connected ? 'text-green-500' : 'text-gray-500'}`}
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M5 13l4 4L19 7"
-            />
-          </svg>
-          <span className={gama.connected ? 'text-green-500' : 'text-gray-500'}>
-            {gama.connected ? 'Connected' : 'Waiting for connection'}
-          </span>
-        </div>
-        
-        {/* display experiment state: */}
-        {renderExperimentState()}
-        <div className="flex justify-center mb-4">
-          <Button onClick={handleTryConnection} text="Try Connection" bgColor="bg-gray-500" icon={
-            <svg
-              className="w-6 h-6 mr-2"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M14.752 11.168l-6.5-3.75A1 1 0 007 8.25v7.5a1 1 0 001.252.832l6.5-3.75a1 1 0 000-1.664z"
-              />
-            </svg>
-          } showText={true} />
-        </div>
-        <div className="flex justify-center space-x-2">
-          <Button onClick={handleLoad} text="Load" bgColor="bg-blue-500" icon={
-            <svg
-              className="w-6 h-6 mr-2"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M12 4v16m8-8H4"
-              />
-            </svg>
-          } showText={true} />
-          <Button onClick={handlePlayPause} text="Play/Pause" bgColor="bg-green-500" icon={
-            <svg
-              className="w-6 h-6 mr-2"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M10 9v6m4-6v6"
-              />
-            </svg>
-          } showText={true} />
-          <Button onClick={handleEnd} text="End" bgColor="bg-red-500" icon={
-            <svg
-              className="w-6 h-6 mr-2"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          } showText={true} />
-        </div>
       </div>
-      <div className="flex justify-center mt-8 space-x-4">
-        
-        {Object.keys(playerList).map((key, index) => {
-          const player = playerList[key];
-          return (
-            <div key={index} className="flex flex-col items-center">
-              <VRHeadset isConnected={player.connected} />
-              <p>{player.connected ? 'Connected' : 'Waiting for connection..'}</p>
-              <div className="flex mt-4 space-x-2">
-                <Button
-                  onClick={() => handleRemove(index)}
-                  text="Remove"
-                  bgColor="bg-red-500"
-                  icon={
-                    <svg
-                      className="w-6 h-6 mr-2"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M6 18L18 6M6 6l12 12"
-                      />
-                    </svg>
-                  }
-                  showText={false}
-                />
-                <Button
-                  onClick={() => handleRestart(index)}
-                  text="Restart"
-                  bgColor="bg-orange-500"
-                  icon={
-                    <svg
-                      className="w-6 h-6 mr-2"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M4 4v6h6M20 20v-6h-6M4 10c1.5-2 4-3 6-3h4c2 0 4 1 5 3M4 14c1.5 2 4 3 6 3h4c2 0 4-1 5-3"
-                      />
-                    </svg>
-                  }
-                  showText={false}
-                />
-              </div>
-            </div>
-          );
-        })}
-      </div>
-          </div>
+    </div>
   );
 };
 
