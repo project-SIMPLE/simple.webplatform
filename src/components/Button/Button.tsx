f2797d76e5219a291da818cde934b116ca068b5b
Tuan <tuan-truong@live.fr>
2024-09-24 15:25:12 +0700

    feat: add footer and delete headset button in Simulation Manager

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -13,9 +13,10 @@ interface ButtonProps {
 
 const Button: React.FC<ButtonProps> = ({ onClick, text, bgColor, icon, showText = true, className }) => {
   return (
+    
     <button
       onClick={onClick}
-      className={`${bgColor} text-white py-2 px-4 rounded-lg flex items-center justify-center gap-2 ${className}`}
+      className={`${bgColor} text-white py-1 px-1.5 rounded-lg flex items-center justify-center gap-2 ${className}`}
     >
       {icon}
       {showText && text}
