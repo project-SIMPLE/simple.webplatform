b0f11d502dc7976446e5d268d70fc344ea59dac7
GuillaumeLeroy <guillaume.leroy34@gmail.com>
2025-02-03 14:55:51 +0700

    first added player component

--- src/components/SimulationManager/SimulationManager.tsx
+++ src/components/SimulationManager/SimulationManager.tsx
@@ -7,7 +7,7 @@ import { useScreenModeState, useScreenModeSetter } from '../ScreenModeContext/Sc
 import MiniNavigation from '../Navigation/MiniNavigation';
 import { useTranslation } from 'react-i18next';
 import Footer from '../Footer/Footer';
-interface Player {
+export interface Player {
   connected: boolean;
   date_connection: string;
   in_game: boolean;
@@ -205,10 +205,9 @@ const SimulationManager = () => {
                                     <path fill="#FFFFFF" d="M 24 4 C 20.491685 4 17.570396 6.6214322 17.080078 10 L 10.238281 10 A 1.50015 1.50015 0 0 0 9.9804688 9.9785156 A 1.50015 1.50015 0 0 0 9.7578125 10 L 6.5 10 A 1.50015 1.50015 0 1 0 6.5 13 L 8.6386719 13 L 11.15625 39.029297 C 11.427329 41.835926 13.811782 44 16.630859 44 L 31.367188 44 C 34.186411 44 36.570826 41.836168 36.841797 39.029297 L 39.361328 13 L 41.5 13 A 1.50015 1.50015 0 1 0 41.5 10 L 38.244141 10 A 1.50015 1.50015 0 0 0 37.763672 10 L 30.919922 10 C 30.429604 6.6214322 27.508315 4 24 4 z M 24 7 C 25.879156 7 27.420767 8.2681608 27.861328 10 L 20.138672 10 C 20.579233 8.2681608 22.120844 7 24 7 z M 11.650391 13 L 36.347656 13 L 33.855469 38.740234 C 33.730439 40.035363 32.667963 41 31.367188 41 L 16.630859 41 C 15.331937 41 14.267499 40.033606 14.142578 38.740234 L 11.650391 13 z M 20.476562 17.978516 A 1.50015 1.50015 0 0 0 19 19.5 L 19 34.5 A 1.50015 1.50015 0 1 0 22 34.5 L 22 19.5 A 1.50015 1.50015 0 0 0 20.476562 17.978516 z M 27.476562 17.978516 A 1.50015 1.50015 0 0 0 26 19.5 L 26 34.5 A 1.50015 1.50015 0 1 0 29 34.5 L 29 19.5 A 1.50015 1.50015 0 0 0 27.476562 17.978516 z"></path>
                                   </svg>
 
-                                }
-                                onClick={togglePopUpshowPopUpManageHeadset}
-                                // onClick={() => handleRemove(key)}
-                            />
+                        }
+                        onClick={togglePopUpshowPopUpManageHeadset}
+                      />
 
                             
                           </div>
@@ -260,63 +259,64 @@ const SimulationManager = () => {
                                 </svg>
                               </p>
 
-                              <div className="flex justify-center space-x-2 gap-10 mb-4 mt-4">
-                                <Button
-                                    onClick={handlePlayPause}
-                                    customStyle={{ width: '100px', height: '50px' }}
-                                    bgColor="bg-green-500"
-                                    showText={true}
-                                    text= {t('button_begin_anyway')}
-                                />
-                              </div>
-                            </>
-                        ) : ( // Autostart simulation
-                            <>
-                              {
-                                  Object.keys(playerList).length >= Number(maxPlayers) && handlePlayPause() // Call handlePlayPause function here
-                              }
-                            </>
-                        )
-                    ) : gama.experiment_state === 'PAUSED' ||
-                    gama.experiment_state === 'LAUNCHING' ||
-                    gama.experiment_state === 'RUNNING' ? (
-                        <>
-                          <div className="flex justify-center space-x-2 gap-10 mb-4 mt-4">
-                            <Button
-                                onClick={handlePlayPause}
-                                customStyle={{width: '100px', height: '50px'}}
-                                bgColor={gama.experiment_state === 'RUNNING' ? 'bg-orange-500' : 'bg-green-500'}
-                                icon={icon}
-                                showText={true}
-                            />
-                            <Button
-                                onClick={handleEnd}
-                                className="w-20"
-                                customStyle={{ width: '100px', height: '50px' }}
-                                bgColor="bg-red-500"
-                                icon={
-                                  <svg className="w-7 h-7" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-                                  </svg>
-                                }
-                                showText={true}
-                            />
-                          </div>
-                          <div className="flex justify-center mt-3 gap-4">
-                            <Button
-                                bgColor={"bg-white"}
-                                showText={true}
-                                className={`border-0 hover:border-none hover:bg-white focus:outline-none ${selectedButton === "gama_screen" ? "" : "opacity-50"}`} // No border or color change on hover
-                                icon={<img src="/images/gama_screen.png" alt="Monitoring" style={{ width: '120px', height: '120px' }} />}
-                                onClick={() => togglePopUp("gama_screen")}
-                            />
-                            <Button
-                                bgColor={"bg-white"}
-                                showText={true}
-                                className={`border-0 hover:border-none hover:bg-white focus:outline-none ${selectedButton === "shared_screen" ? "" : "opacity-50"}`} // No border or color change on hover
-                                icon={<img src="/images/shared_screen.png" alt="shared_screen" style={{ width: '120px', height: '120px' }} />}
-                                onClick={() => togglePopUp("shared_screen")}
-                            />
+                      <div className="flex justify-center space-x-2 gap-10 mb-4 mt-4 w-fit">
+                        <Button
+                          onClick={handlePlayPause}
+                          bgColor="bg-green-500"
+                          customStyle={{ height: '50px' }}
+                          showText={true}
+                          text={t('button_begin_anyway')}
+                        />
+                      </div>
+                    </>
+                  ) : ( // Autostart simulation
+                    <>
+                      {
+                        Object.keys(playerList).length >= Number(maxPlayers) && handlePlayPause() // Call handlePlayPause function here
+                      }
+                    </>
+                  )
+                ) : gama.experiment_state === 'PAUSED' ||
+                  gama.experiment_state === 'LAUNCHING' ||
+                  gama.experiment_state === 'RUNNING' ? (
+                  <>
+                    <div className="flex justify-center space-x-2 gap-10 mb-4 mt-4">
+                      <Button
+                        onClick={handlePlayPause}
+                        customStyle={{ width: '100px', height: '50px' }}
+                        bgColor={gama.experiment_state === 'RUNNING' ? 'bg-orange-500' : 'bg-green-500'}
+                        icon={icon}
+                        showText={true}
+                      />
+                      <Button
+                        onClick={handleEnd}
+                        className="w-20"
+                        customStyle={{ width: '100px', height: '50px' }}
+                        bgColor="bg-red-500"
+                        icon={
+                          <svg className="w-7 h-7" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                          </svg>
+                        }
+                        showText={true}
+                      />
+                    </div>
+                    <div className="flex justify-center mt-3 gap-4">
+                      <Button
+                        bgColor={"bg-white"}
+                        showText={true}
+                        className={`border-0 hover:border-none hover:bg-white focus:outline-none ${screenModeDisplay === "gama_screen" ? "" : "opacity-50"}`} // No border or color change on hover
+                        icon={<img src="/images/gama_screen.png" alt="Monitoring" style={{ width: '120px', height: '120px' }} />}
+                        onClick={() => ("gama_screen")}
+                      />
+                      <Button
+                        bgColor={"bg-white"}
+                        showText={true}
+                        className={`border-0 hover:border-none hover:bg-white focus:outline-none ${screenModeDisplay === "shared_screen" ? "" : "opacity-50"}`} 
+                        icon={<img src="/images/shared_screen.png" alt="shared_screen" style={{ width: '120px', height: '120px' }} />}
+                        onClick={() => updateDisplay("shared_screen")}
+                      />
+                     <p>{screenModeDisplay}</p>
 
 
                           </div>
---
+++ src/components/SimulationManager/SimulationManagerPlayer.tsx
@@ -0,0 +1,94 @@
+import VRHeadset from "../VRHeadset/VRHeadset";
+import { useTranslation } from "react-i18next";
+import Footer from "../Footer/Footer";
+import Button from "../Button/Button";
+
+
+type PlayerProps = {
+  key: string;
+  player?: any;
+  handleRestart: (id: string) => void;
+  handleRemove: (id: string) => void;
+  togglePopUpshowPopUpManageHeadset: () => void;
+}
+
+const Player = ({ handleRestart, handleRemove, togglePopUpshowPopUpManageHeadset, key, player }: PlayerProps) => {
+  const {t} = useTranslation();
+
+  {
+    // Object.keys(playerList).map((key) => {
+    //   const player = playerList[key]; //TODO enlever ces commentaires
+      return (
+        <div key={key} className="flex flex-col items-center" >
+
+          <VRHeadset
+            key={key}
+            selectedPlayer={player}
+            playerId={key} />
+
+          <>
+            {/* Grey Overlay */}
+            <div className="fixed inset-0 bg-gray-800 bg-opacity-75 z-50"  ></div>
+
+            <div className="fixed inset-0 flex items-center justify-center z-50" onClick={togglePopUpshowPopUpManageHeadset}  >
+              <div className="bg-white p-6 rounded-lg shadow-lg w-72 text-center"  >
+                <h2 className="text-lg font-semibold mb-4"  >
+                  {t('popop_question')} {key} ?
+                </h2>
+
+                <div className='flex gap-5 ml-3'  >
+
+                  <button
+                    className="bg-red-500 text-white px-4 py-2 mt-4 rounded"
+                    onClick={() => handleRemove(key)}  >
+
+                    {t('remove')}
+                  </button>
+
+                  <button
+                    className="bg-orange-500 text-white px-4 py-2 mt-4 rounded"
+                    onClick={() => handleRestart(key)}  >
+
+                    {t('relaunch')}
+                  </button>
+
+                </div>
+
+
+
+                <button
+                  className="bg-red-500 text-white px-4 py-2 mt-6 rounded"
+                  onClick={togglePopUpshowPopUpManageHeadset}  >
+
+                  {t('cancel')}
+                </button>
+              </div>
+            </div>
+          </>
+
+          {/* The trash */}
+          <div className='flex gap-3 mt-2'  >
+            <p style={{ marginTop: '3px' }}  > {key} </p>
+            <Button
+              bgColor='bg-red-500'
+              icon={
+                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="18" height="18" viewBox="0 0 48 48"  >
+                  <path fill="#FFFFFF" d="M 24 4 C 20.491685 4 17.570396 6.6214322 17.080078 10 L 10.238281 10 A 1.50015 1.50015 0 0 0 9.9804688 9.9785156 A 1.50015 1.50015 0 0 0 9.7578125 10 L 6.5 10 A 1.50015 1.50015 0 1 0 6.5 13 L 8.6386719 13 L 11.15625 39.029297 C 11.427329 41.835926 13.811782 44 16.630859 44 L 31.367188 44 C 34.186411 44 36.570826 41.836168 36.841797 39.029297 L 39.361328 13 L 41.5 13 A 1.50015 1.50015 0 1 0 41.5 10 L 38.244141 10 A 1.50015 1.50015 0 0 0 37.763672 10 L 30.919922 10 C 30.429604 6.6214322 27.508315 4 24 4 z M 24 7 C 25.879156 7 27.420767 8.2681608 27.861328 10 L 20.138672 10 C 20.579233 8.2681608 22.120844 7 24 7 z M 11.650391 13 L 36.347656 13 L 33.855469 38.740234 C 33.730439 40.035363 32.667963 41 31.367188 41 L 16.630859 41 C 15.331937 41 14.267499 40.033606 14.142578 38.740234 L 11.650391 13 z M 20.476562 17.978516 A 1.50015 1.50015 0 0 0 19 19.5 L 19 34.5 A 1.50015 1.50015 0 1 0 22 34.5 L 22 19.5 A 1.50015 1.50015 0 0 0 20.476562 17.978516 z M 27.476562 17.978516 A 1.50015 1.50015 0 0 0 26 19.5 L 26 34.5 A 1.50015 1.50015 0 1 0 29 34.5 L 29 19.5 A 1.50015 1.50015 0 0 0 27.476562 17.978516 z"  ></path>
+                </svg>
+
+              }
+              onClick={togglePopUpshowPopUpManageHeadset}
+            // onClick={() => handleRemove(key)}
+            />
+
+
+          </div>
+          <Footer />
+        </div>
+      );
+
+    }}
+
+
+
+export default Footer;
