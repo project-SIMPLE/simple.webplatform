a990707db2a13a7e4b935238ed244bb4b3872a2f
GuillaumeLeroy <guillaume.leroy34@gmail.com>
2025-02-20 14:01:55 +0700

    disabled verbose logs on the websocket server

--- src/server.js
+++ src/server.js
@@ -14,16 +14,16 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-  console.log(`User connected: ${socket.id}`);
+  // console.log(`User connected: ${socket.id}`);
 
   // Listen for messages
   socket.on("message", (data) => {
-    console.log(`Received message: ${data}`);
+    // console.log(`Received message: ${data}`);
     io.emit("message", data); // Broadcast message
   });
 
   socket.on("disconnect", () => {
-    console.log(`User disconnected: ${socket.id}`);
+    // console.log(`User disconnected: ${socket.id}`);
   });
 });
 
