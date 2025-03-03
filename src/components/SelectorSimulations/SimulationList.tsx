90f3cf57759debe1d2dcc0db7311c468a5feb0b1
GuillaumeLeroy <guillaume.leroy34@gmail.com>
2025-03-03 15:07:13 +0700

    added new component for simulation elements

---
+++ src/components/SelectorSimulations/SimulationList.tsx
@@ -0,0 +1,47 @@
+
+
+interface SimulationListProps{
+    list: any[];
+    handleSimulation: any;
+    gama: any;
+}
+
+const SimulationList = ({list, handleSimulation, gama}: SimulationListProps ) => {
+
+
+return (
+<div className="flex mt-5 mb-8" style={{ gap: '55px' }}>
+              
+              { list.map((simulation, index) => (
+                <div
+                  className={`bg-white shadow-lg rounded-3xl p-6 flex flex-col items-center h-40 cursor-pointer ${!gama.connected ? 'opacity-50 cursor-not-allowed' : ''
+                    }`}
+
+                  style={{
+                    backgroundImage: simulation.type == "json_settings" ? `url(${simulation.splashscreen})` : `url(/images/codecode.png)`,
+                    backgroundSize: 'cover',
+                    width: '100px',
+                    height: '100px',
+                    zIndex: 1, 
+                  }}
+
+
+                  key={index}
+                  onClick={gama.connected ? () => handleSimulation(index) : () => { }}
+                >
+                  <h2
+                    className="text-gray-500 text-sm text-center mt"
+                    style={{
+                      marginTop: '80px',
+                    }}
+                  >
+                    {simulation.type == "json_settings" ? simulation.name : "sub-projects"}
+                  </h2>
+                </div>
+              ))}
+            </div>)
+
+}
+
+
+export default SimulationList
