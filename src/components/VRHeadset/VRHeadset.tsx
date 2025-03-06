389626a6913cba4f339f2356f46f825f8b6357a9
GuillaumeLeroy <guillaume.leroy34@gmail.com>
2025-03-06 11:40:52 +0700

    removed unused player informations, now displayed in the SimulationManagerPlayer component

--- src/components/VRHeadset/VRHeadset.tsx
+++ src/components/VRHeadset/VRHeadset.tsx
@@ -9,8 +9,6 @@ interface VRHeadsetProps {
 }
 
 const VRHeadset= ({ selectedPlayer, className, playerId }: VRHeadsetProps) => {
-  const [showPopUp, setShowPopUp] = useState(false);
-  const {t} = useTranslation();
 
   // Determines if the player is available 
   const isAvailable = !!selectedPlayer;
@@ -29,46 +27,22 @@ const VRHeadset= ({ selectedPlayer, className, playerId }: VRHeadsetProps) => {
         }
     };
 
-  const togglePopUp = () => {
-    setShowPopUp(!showPopUp); 
-  };
 
   return (
     <>
       <div
-        className={`flex flex-col items-center ${className} ${isAvailable ? 'grayscale-0' : 'opacity-50 cursor-not-allowed'}`}
+        className={`flex flex-col items-center  ${className} ${isAvailable ? 'grayscale-0' : 'opacity-50 cursor-not-allowed'}`}
         style={{ transition: 'all 0.3s ease', cursor: isAvailable ? 'pointer' : 'not-allowed' }}
       >
         <img 
           src={getHeadsetColor()}
           alt="VR Headset"
-           className={`w-32 h-32 object-cover mb-2 border-black border- `}
+           className={`w-32 h-32 object-cover border-black border- `}
 
         />
 
-      {/* Pop-up pour afficher les informations du joueur */}
-      </div>
-        
-      {showPopUp && isAvailable && (
-        <>
 
-          <div className="fixed inset-0 flex items-center justify-center z-50 bg-slate-800 opacity-75">
-              <div className="bg-white p-6 rounded-lg shadow-lg w-72 text-center">
-                  <h2 className="text-lg font-semibold mb-4">Player Informations</h2>
-                  <p>Player: {String(playerId)}</p>
-                  <p>{t('Status')} : {String(selectedPlayer.connected)}</p>
-                  <p>{t('Hour of connection')} : {selectedPlayer.date_connection}</p>
-                  <p>{t('In game')} : {String(selectedPlayer.in_game)}</p>
-
-                  <button
-                      className="bg-red-500 text-white px-4 py-2 mt-4 rounded"
-                  >
-                      {t('cancel')}
-                  </button>
-              </div>
-          </div>
-        </>
-      )}
+    </div>
     </>
   );
 };
