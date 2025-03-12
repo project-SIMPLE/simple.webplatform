f77ae23b24302c74cd90533e2b778a4672e80b62
GuillaumeLeroy <guillaume.leroy34@gmail.com>
2025-03-12 15:51:21 +0700

    fixed gama project launching twice because of strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -32,8 +32,8 @@ const container = document.getElementById('root')!;
 const root = ReactDOM.createRoot(container);
 
 root.render(
-   <React.StrictMode>
+   // <React.StrictMode>
       <App />
-    </React.StrictMode> 
+   // </React.StrictMode> 
 );
 
