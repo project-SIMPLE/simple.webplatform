128a66c844106344bd491413956b92885b32bf1f
RoiArthurB <contact@arthurbrugiere.fr>
2025-09-17 17:34:16 +0700

    feat: Implement evilscan library Reworking entirely the DeviceFinder class - Simplify the overall class since we're not OS dependent anymore - Enforce proper verbose logs (it's quite low level and not everyone care about this class) - Keep pretty much the same algorithm, just relying on evilscan callbacks   - Parsing both port 5555 (default) and random range (30k-50k) and connect to the first open port there   - The timeout is a little short and can (occasionally) skip a port, but the detection is fast enough to not be a real problem
    = Overall migration improvement has been benchmarked as 2.33 ± 0.66 times faster than the custom script

--- src/api/android/adb/DeviceFinder.ts
+++ src/api/android/adb/DeviceFinder.ts
@@ -1,24 +1,22 @@
 import { spawn } from 'child_process';
-import path from 'path';
+import Evilscan from "evilscan";
 
 import Controller from "../../core/Controller.ts";
 import {HEADSETS_IP, ENV_EXTRA_VERBOSE, ENV_VERBOSE} from "../../index.ts";
 
 class DeviceFinder {
     controller: Controller;
-    private scriptPath: string;
     ipToConnect: string[];
     isScanning: boolean = false;
 
     constructor(controller: Controller) {
         this.controller = controller;
-        this.scriptPath = path.join(process.cwd(), 'toolkit', 'scan_and_connect'); // the extension of the file is added later to select either zsh or ps1
         this.ipToConnect = HEADSETS_IP;
 
         // Filter out already connected IPs
         const clientStreaming = this.controller.adb_manager.clientCurrentlyStreaming;
         this.ipToConnect = this.ipToConnect.filter(ip => {
-            return !clientStreaming.some(item => item.startsWith(ip));
+            return !clientStreaming.some(item => item.serial.startsWith(ip));
         });
         if (ENV_VERBOSE) console.log("[ADB FINDER] Loaded successfully, will start to scan for devices now...");
     }
@@ -26,9 +24,8 @@ class DeviceFinder {
     public async scanAndConnect() {
         if (this.ipToConnect.length === 0 || this.isScanning) {
             if(this.ipToConnect.length === 0)
-            {
-                console.log('[ADB FINDER] Every known IP already connected, stopping now...');
-            } else
+                if (ENV_VERBOSE) console.log('[ADB FINDER] Every known IP already connected, stopping now...');
+            else
                 if (ENV_EXTRA_VERBOSE) console.log('[ADB FINDER] Already scanning for new IP, skipping this call...');
 
             return;
@@ -37,26 +34,20 @@ class DeviceFinder {
         this.isScanning = true; // Set the flag before starting to connect, otherwise, multiple attempts will start concurrently before the flag will be set.
 
         try {
-            
-            console.log('[ADB FINDER] Start looking to connect for those IP : ', this.ipToConnect);
+            if (ENV_VERBOSE) console.log('[ADB FINDER] Start looking to connect for those IP : ', this.ipToConnect);
 
             for (let i = 0; i < this.ipToConnect.length; i++) { // Directly use this.ipToConnect. No need to copy
                 const ip = this.ipToConnect[i];
                 if (ENV_VERBOSE) console.log('[ADB FINDER] Trying ', ip);
 
                 try {
-                    const output = await this.scanAndConnectIP(ip);
+                    const output: boolean = await this.scanAndConnectIP(ip);
 
-                    if (output.includes('OK')) {
-                        console.log('[ADB FINDER] Successfully connected to ', ip);
-                        await this.controller.adb_manager.startStreaming(ip);
-                        this.ipToConnect.splice(i, 1); // Remove the connected IP; adjust index
-                        i--;                     // Decrement i to account for removed element
-                    } else if (output.includes('ERROR')) {
-                        console.warn('[ADB FINDER] Failed to connect to ' + ip);
-                        if (ENV_VERBOSE) console.warn(output);
+                    if (output) { //.includes('OK')
+                        if (ENV_VERBOSE) console.log('[ADB FINDER] Successfully connected to ', ip);
+                        this.ipToConnect.splice(i--, 1); // Remove the connected IP; adjust index
                     } else {
-                        console.error('[ADB FINDER] Unknown message:', output);
+                        if (ENV_VERBOSE) console.warn('[ADB FINDER] Failed to connect to ' + ip);
                     }
 
                 } catch (innerError) {
@@ -68,8 +59,8 @@ class DeviceFinder {
 
                 this.isScanning = false;  // Allow new thread to search for devices
 
-                console.log('[ADB FINDER] Those IP are left to be connected : ', this.ipToConnect);
-                console.log('[ADB FINDER] Retry in 5 seconds...');
+                if (ENV_VERBOSE) console.log('[ADB FINDER] Those IP are left to be connected : ', this.ipToConnect);
+                if (ENV_VERBOSE) console.log('[ADB FINDER] Retry in 5 seconds...');
 
                 // Trigger new call
                 setTimeout(async () => {
@@ -77,60 +68,54 @@ class DeviceFinder {
                 }, 5000);
 
             } else {
-                console.log('[ADB FINDER] All devices connected.');
-                console.log('[ADB FINDER] Stopping now...');
+                if (ENV_VERBOSE) console.log('[ADB FINDER] All devices connected.');
+                if (ENV_VERBOSE) console.log('[ADB FINDER] Stopping now...');
             }
         }
     }
 
 
-    public async scanAndConnectIP(ipAddress: string): Promise<string[]> {
-        if(process.platform === 'win32'){
-            return new Promise((resolve, reject) => {
-                const parameters = ['-ip_adresses', ipAddress, // Replace with your IP addresses (comma-separated if multiple)
-                                    ]
-                const child = spawn('powershell', [this.scriptPath + '.ps1', ...parameters]);
-                let outputLines: string[] = []; // Store output lines
+    public async scanAndConnectIP(ipAddress: string): Promise<boolean> {
+        let alreadyConnected: boolean = false;
+        let finishedScanning: boolean = false;
 
-                child.stdout.on('data', (data) => {
-                    const lines = data.toString().split('\n');
-                    outputLines.push(...lines);
-                });
+        const options = {
+            target: ipAddress,
+            port: '5555,30000-49999',   // your custom range
+            // port: '1-65535',
+            status: 'O',                // 'TROU' : Timeout, Refused, Open, Unreachable
+            concurrency: 500,           // how many ports to test in parallel
+            timeout: 250,               // maximum number of milliseconds before closing the connection
+            banner:false
+        };
 
-                child.stderr.on('data', (data) => {
-                    console.error('Script Error:', data.toString());
-                });
+        new Evilscan(options)
+            .on('result', async (data:any) => {
+                if (ENV_EXTRA_VERBOSE) console.log('[ADB FINDER - EvilScan] === Scan of ', ipAddress,' find this:', data);
+                if (!alreadyConnected){
+                    if (ENV_EXTRA_VERBOSE) console.log('[ADB FINDER - EvilScan] === Trying to ADB connect to', data.ip, ':', data.port);
 
-                child.on('close', (code) => {
-                    if (code === 0) {
-                        resolve(outputLines);
-                    } else {
-                        reject(`Script failed with exit code ${code}`);
+                    try {
+                        alreadyConnected = await this.controller.adbConnectNewDevice(data.ip, data.port);
+                    } catch (e) {
+                        if (ENV_EXTRA_VERBOSE) console.error("[ADB FINDER - EvilScan] === Couldn't connect with this error message", e)
                     }
-                });
-            });
-        }else{
-        return new Promise((resolve, reject) => {
-            const child = spawn('zsh', [this.scriptPath + ".zsh", ipAddress]);
-            let outputLines: string[] = []; // Store output lines
-
-            child.stdout.on('data', (data) => {
-                const lines = data.toString().split('\n');
-                outputLines.push(...lines);
-            });
-
-            child.stderr.on('data', (data) => {
-                console.error('Script Error:', data.toString());
-            });
-
-            child.on('close', (code) => {
-                if (code === 0) {
-                    resolve(outputLines);
                 } else {
-                    reject(`Script failed with exit code ${code}`);
+                    if (ENV_EXTRA_VERBOSE) console.log('[ADB FINDER - EvilScan] === Already connected, skipping', data.ip, ':', data.port);
                 }
-            });
-        });}
+            })
+            .on('done', () => {
+                if (ENV_EXTRA_VERBOSE) console.log('[ADB FINDER - EvilScan] === Scan of ', ipAddress,' completed.');
+                finishedScanning = true;
+            })
+            .run();
+
+        // Dirty waiting for scan to finish
+        while(!finishedScanning) {
+            await new Promise(f => setTimeout(f, 1000));
+        }
+
+        return alreadyConnected;
     }
 }
 
