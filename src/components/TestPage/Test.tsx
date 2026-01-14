0947379aea3d6cce1105d08d15b71e1d32380e11
GuillaumeLeroy <guillaume.leroy34@gmail.com>
2026-01-14 10:32:09 +0700

    added "Test" page to test canvas changes

---
+++ src/components/TestPage/Test.tsx
@@ -0,0 +1,18 @@
+import VideoStreamManager from "../WebSocketManager/VideoStreamManager";
+
+
+const Test = ({}) => {
+
+    return (
+        <div className="size-full bg-blue-200">
+
+
+
+            <VideoStreamManager needsInteractivity="false"/>
+
+        </div>
+
+
+    );
+};
+export default Test;
