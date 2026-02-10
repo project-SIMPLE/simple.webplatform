2de8f09ab03f70005c6176c9a10dc10c45e13c78
GuillaumeLeroy <guillaume.leroy34@gmail.com>
2026-02-10 09:47:59 +0700

    Fixed downward arrow being wrongfully displayed in a flex, causing it to warp the splashscreen, the latter not being displayed in the whole container and being squished instead

--- src/components/SelectorSimulations/SimulationList.tsx
+++ src/components/SelectorSimulations/SimulationList.tsx
@@ -10,10 +10,10 @@ interface SimulationListProps {
     experiment_state: string;
     experiment_name: string;
     content_error: string;
-}
+  }
   className?: string;
 }
-    const logger = getLogger(["components", "SimulationList"]);
+const logger = getLogger(["components", "SimulationList"]);
 
 const SimulationList = ({ list, handleSimulation, gama, className }: SimulationListProps) => {
 
@@ -22,35 +22,35 @@ const SimulationList = ({ list, handleSimulation, gama, className }: SimulationL
     <div className="flex mt-5 mb-8" style={{ gap: '55px' }}>
 
       {list.map((simulation, index) => (
-        <div className='items-center text-center w-24' key={index}>       
+        <div className='items-center text-center w-24 ' key={index}>
           <div
-          className={`shadow-lg rounded-2xl flex flex-col items-center h-40 cursor-pointer bg-slate-100
+            className={`shadow-lg rounded-2xl items-center h-40 cursor-pointer bg-slate-100 relative
                     ${className} 
                     ${!gama.connected ? 'opacity-50' : null} 
                     ${simulation.type == "catalog"}  
                     `}
 
-          style={{
-            width: '100px',
-            height: '100px',
-            zIndex: 1,
-          }}
+            style={{
+              width: '100px',
+              height: '100px',
+              zIndex: 1,
+            }}
 
 
-          key={index}
-          onClick={gama.connected ? () => handleSimulation(index) : () => { }}
-        >
-          {simulation.type == "catalog" ? <img src={arrow_down} className='rounded-full bg-slate-500 opacity-90 size-16' /> : null} {/* //? downward arrow */}
-          <img src={` ${simulation.splashscreen}`} 
-          onError={(e) => { e.target.src = "/images/simple_logo.png"; logger.warn("couldn't load an image for simulation {index}, using the placeholder",{index})}}
-          className='size-full rounded-2xl'
-          />
+            key={index}
+            onClick={gama.connected ? () => handleSimulation(index) : () => { }}
+          >{/*The absolute positionning guarantees that the image is the only static child so it takes the whole screen top and left are there to position the down arrow */}
+            {simulation.type == "catalog" ? <img src={arrow_down} className='rounded-full bg-slate-500 opacity-90 size-16 absolute top-[18px] left-[18px]' /> : null} {/* //? downward arrow */}
+            <img src={` ${simulation.splashscreen}`}
+              onError={(e) => { e.target.src = "/images/simple_logo.png"; logger.warn("couldn't load an image for simulation {index}, using the placeholder", { index }) }}
+              className='size-full rounded-2xl'
+            />
 
 
 
-        </div>
-        
-        <h2
+          </div>
+
+          <h2
             className="text-gray-500 text-sm text-center mt"
           >
             {/*                                                                                                                     ↓ added one for folders to start at 1 instead of 0 */}
