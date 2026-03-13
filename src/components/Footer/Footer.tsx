8300ce67b4f8ea38519f66bfbd4655a08b5bb9a7
GuillaumeLeroy <guillaume.leroy34@gmail.com>
2026-03-13 11:57:29 +0700

    Changed logos in the footer to use the new ones

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,17 +1,19 @@
+
+const folder = process.env.IMAGE_SOURCE_FOLDER;
 const Footer = () => {
     return (
 
 
         <footer className="flex justify-between items-center p-4 border-t border-gray-300  w-full">
         <div className='flex'>
-          <img src="/images/global-gateway-euro.png" alt="Global Gateway" className="h-8" />
-          <img src="/images/funded-by-ue.png" alt="Global Gateway" className="h-8" />
+          <img src={`images/${folder}/Logos/Logo_Global_gateway.png`} alt="GlobalGateway" className="h-12" />
+          <img src={`images/${folder}/Logos/Logo_UE.png`} alt="UE" className="h-12" />
         </div>
 
         <div className='flex gap-3' >
-          <img src="images/IRD-logo.png" alt="IRD" className="h-8" />
-          <img src="images/nstda-logo.png" alt="CTU" className="h-8" />
-          <img src="images/ctu-logo.png" alt="NSTDA" className="h-8" />
+          <img src={`images/${folder}/Logos/Logo_IRD.png`} alt="IRD" className="h-12" />
+          <img src={`images/${folder}/Logos/Logo_NSTDA.png`} alt="NSTDA" className="h-12" />
+          <img src={`images/${folder}/Logos/Logo_Can_Tho.png`} alt="CTU" className="h-12" />
         </div>
       </footer>
 
