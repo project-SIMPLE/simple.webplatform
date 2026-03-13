b05ae60ae9bc440691b80af6d112d1c0c5cca031
GuillaumeLeroy <guillaume.leroy34@gmail.com>
2026-03-13 11:57:52 +0700

    changed the language selector to use the new images, changed button to clickable images

--- src/components/LanguageSelector/LanguageSelector.tsx
+++ src/components/LanguageSelector/LanguageSelector.tsx
@@ -1,11 +1,9 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import letterswitch from '/src/svg_logos/letter_switch.svg';
-import x_cross from '/src/svg_logos/x_cross.svg';
+const folder = process.env.IMAGE_SOURCE_FOLDER;
 const LanguageSelector = () => {
   const { t, i18n } = useTranslation();
   const [showPopup, setShowPopup] = useState(false);
-  const button_green = "px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 transition duration-300"; //factorisation of button attributes for better readability and ease of modification
 
   const changeLanguage = (lng: string) => {
     i18n.changeLanguage(lng);
@@ -18,60 +16,68 @@ const LanguageSelector = () => {
 
   return (
 
-    <div>
+    <div className='flex flex-col  h-full justify-center'>
       {/* Button to open the popup */}
-      <button
-        onClick={() => setShowPopup(true)}
-        className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition duration-300 flex items-center"
-      >
-        {t('change_language')}
-        <img src={letterswitch} alt="switch language" className='ml-2' />
-      </button>
+        <img src={`public/images/${folder}/Language_selection/Language_selection_Button_00.png`} alt="language selection" className='size-14 cursor-pointer' onClick={()=>{setShowPopup(true)}}/>
 
 
       {/* Pop-up */}
       {showPopup && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-10" onClick={() => setShowPopup(false)}>
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-30" onClick={() => setShowPopup(false)}>
 
-          <div className="bg-white p-6 rounded-lg shadow-lg w-80" onClick={(e) => e.stopPropagation()}
+          <div className=" p-6 rounded-lg w-80" onClick={(e) => e.stopPropagation()}
             style={{ position: 'relative', top: '0', right: '0' }}
-            >
+          >
             <button
               onClick={() => setShowPopup(false)}
-              className='bg-white hover:bg-gray-200 rounded-full p-1 size-fit'
+              className=' rounded-full p-1 size-fit bg-none z-10'
               style={{ position: 'absolute', top: '0.5rem', right: '0.5rem' }}
             >
-              <img src={x_cross} alt="" className='mix-blend-difference size-8' />
+              <img src={`public/images/${folder}/Language_selection/Language_selection_close.png`} alt="" className='mix-blend-difference size-8' />
             </button>
             <h3 className="text-lg font-semibold mb-4">{t('select_language')}</h3>
 
 
+            <img src={`public/images/${folder}/Language_selection/Language_selection_tab.png`} className="absolute size-full inset-0 objec-cover" alt="" />
 
-            <div className="flex flex-col space-y-2">
-              <button
-                onClick={() => changeLanguage('en')}
-                className={button_green}
-              >
-                English
-              </button>
-              <button
-                onClick={() => changeLanguage('fr')}
-                className={button_green}
-              >
-                Français
-              </button>
-              <button
-                onClick={() => changeLanguage('vn')}
-                className={button_green}
-              >
-                Việt
-              </button>
-              <button
-                onClick={() => changeLanguage('th')}
-                className={button_green}
-              >
-                แบบไทย
-              </button>
+            <div className="flex flex-col space-y-2 relative">
+
+              <div className='relative flex items-center justify-center cursor-pointer transition-transform duration-300 hover:scale-105'>
+                <img
+                  src={`public/images/${folder}/Language_selection/Language_selection_Button_01.png`}
+                  alt="English"
+                  onClick={() => changeLanguage('en')}
+                />
+                <p className='absolute pointer-events-none'>English</p>
+              </div>
+              <div className='relative flex items-center justify-center cursor-pointer transition-transform duration-300 hover:scale-105'>
+                <img
+                  src={`public/images/${folder}/Language_selection/Language_selection_Button_02.png`}
+                  alt="Français"
+                  onClick={() => changeLanguage('fr')}
+                />
+                <p className='absolute pointer-events-none'>Français</p>
+              </div>
+              <div className='relative flex items-center justify-center cursor-pointer transition-transform duration-300 hover:scale-105'>
+                <img
+                  src={`public/images/${folder}/Language_selection/Language_selection_Button_01.png`}
+                  alt="English"
+                  onClick={() => changeLanguage('vn')}
+                />
+                <p className='absolute pointer-events-none'>Việt</p>
+              </div>
+              <div className='relative flex items-center justify-center cursor-pointer transition-transform duration-300 hover:scale-105'>
+                <img
+                  src={`public/images/${folder}/Language_selection/Language_selection_Button_01.png`}
+                  alt="English"
+                  onClick={() => changeLanguage('th')}
+                />
+                <p className='absolute pointer-events-none'>แบบไทย</p>
+              </div>
+
+
+
+
             </div>
 
 
