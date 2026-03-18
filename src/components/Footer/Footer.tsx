3970e35cfd9053945c8cdebbf679306cfb8c1ea3
GuillaumeLeroy <guillaume.leroy34@gmail.com>
2026-03-18 12:06:24 +0700

    manually adjusted the layout of the footer to be more harmonious

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,24 +1,24 @@
-
+const folder = process.env.IMAGE_SOURCE_FOLDER
 const Footer = () => {
-    return (
+  return (
 
 
-        <footer className="flex justify-between items-center p-4 w-full">
-        <div className='flex gap-3'>
-          <img src={`images/global-gateway-euro.png`} alt="GlobalGateway" className="h-12" />
-          <img src={`images/funded-by-ue.png`} alt="UE" className="h-12" />
-        </div>
+    <footer className="flex justify-between items-center w-full relative m-[50px]">
+      <img src={`public/images/${folder}/Frames/footer.svg`} alt="" className="absolute"/>
+      <div className='flex gap-3 z-10 ml-[10px]'>
+        <img src={`images/global-gateway-euro.png`} alt="GlobalGateway" className="h-12" />
+        <img src={`images/funded-by-ue.png`} alt="UE" className="h-12" />
+      </div>
 
-        <div className='flex gap-[50px]' >
-          <img src={`images/IRD-logo.png`} alt="IRD" className="h-12" />
-          <img src={`images/nstda-logo.png`} alt="NSTDA" className="h-12" />
-          <img src={`images/ctu-logo.png`} alt="CTU" className="h-12" />
-        </div>
-      </footer>
+      <div className='flex gap-[50px] z-10 mr-[50px]' >
+        <img src={`images/IRD-logo.png`} alt="IRD" className="h-12" />
+        <img src={`images/nstda-logo.png`} alt="NSTDA" className="h-12" />
+        <img src={`images/ctu-logo.png`} alt="CTU" className="h-12" />
+      </div>
+    </footer>
 
-    
-      );
-    };
-    
-    export default Footer;
-    
+
+  );
+};
+
+export default Footer;
