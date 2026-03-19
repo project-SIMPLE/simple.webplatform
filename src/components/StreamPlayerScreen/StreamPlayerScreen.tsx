3b120ef70748df3fb6b55f659e617545d2ee0b4b
GuillaumeLeroy <guillaume.leroy34@gmail.com>
2026-03-19 15:37:31 +0700

    fixed container not occupying the whole screen on mac OS

--- src/components/StreamPlayerScreen/StreamPlayerScreen.tsx
+++ src/components/StreamPlayerScreen/StreamPlayerScreen.tsx
@@ -10,7 +10,7 @@ const StreamPlayerScreen = () => {
     <div className="w-full h-full flex flex-col items-center justify-around">
 
       <div className="w-full h-full flex flex-col items-center">
-          <div className="flex justify-center items-center h-screen" ref={videoContainerRef}>
+          <div className="flex justify-center items-center h-screen w-full" ref={videoContainerRef}>
             <VideoStreamManager />
             <div className='flex flex-col'>
             </div>
