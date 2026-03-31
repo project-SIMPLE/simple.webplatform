8cc2100af0fafc021bcf816a6f61aa10d8154515
GuillaumeLeroy <guillaume.leroy34@gmail.com>
2026-03-31 15:28:11 +0700

    adjusted header to use dynamic units

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,13 +9,13 @@ const Header = () => {
     return (
         <div className="flex w-full justify-between align-middle relative" >
             {folder === "V2" &&
-             <img src={`/images/${folder}/Frames/header_simple.svg`} className="w-[600px] h-auto absolute left-[0px] top-[-100px]" alt="" /> 
+             <img src={`/images/${folder}/Frames/header_simple.svg`} className="h-[10dvw] w-auto absolute left-[0px] top-[-100px]" alt="" /> 
             }
             <Link to="/" className="text-white hover:text-gray-400 z-10">
-                <img src={logoSimple} alt="Logo" className=" ml-10 w-[515px]  mt-12 pl-4" />
+                <img src={logoSimple} alt="Logo" className=" ml-10 h-[6dvh]  mt-12 pl-4 hover:scale-110 transition-transform duration-200" />
             </Link>
             {folder === "V2" &&
-            <img src={`/images/${folder}/Frames/header_language.svg`} className="w-[550px] h-auto absolute right-[-200px] top-[-70px]" alt="" />
+            <img src={`/images/${folder}/Frames/header_language.svg`} className="h-[10dvh] w-auto absolute right-[-200px] top-[-70px]" alt="" />
             }
 
             {/* ↓ this div is the white vertical separator on screen */}
