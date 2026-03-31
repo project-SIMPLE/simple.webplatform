2bf620c9ad7525734de8402175ea6d7e0a455d78
GuillaumeLeroy <guillaume.leroy34@gmail.com>
2026-03-31 15:27:54 +0700

    Adjusted footer to use dynamic units instead of pixels

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,16 +3,16 @@ const Footer = () => {
   return (
 
 
-    <footer className="flex justify-between items-center w-full relative  mb-[3 0px]">
+    <footer className="flex justify-between items-center w-full relative  mb-[3dvh]">
       {folder === "V2" &&
-      <img src={`/images/${folder}/Frames/footer.svg`} alt="" className="absolute"/>
+      <img src={`/images/V2/Frames/footer.svg`} alt="" className="absolute"/>
       }
-      <div className='flex gap-3 z-10 ml-[10px]'>
+      <div className='flex gap-3 ml-[10px]'>
         <img src={`images/global-gateway-euro.png`} alt="GlobalGateway" className="h-12" />
         <img src={`images/funded-by-ue.png`} alt="UE" className="h-12" />
       </div>
 
-      <div className='flex gap-[50px] z-10 mr-[50px]' >
+      <div className='flex gap-[50px] mr-[50px]' >
         <img src={`images/IRD-logo.png`} alt="IRD" className="h-12" />
         <img src={`images/nstda-logo.png`} alt="NSTDA" className="h-12" />
         <img src={`images/ctu-logo.png`} alt="CTU" className="h-12" />
