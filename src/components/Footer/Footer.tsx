5b46579ac8a00ebd820f56452418545a3eeccf5a
GuillaumeLeroy <guillaume.leroy34@gmail.com>
2026-04-09 13:59:43 +0700

    adjusted the margins on the footer to have the same as the header

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,14 +4,14 @@ const Footer = () => {
 
 
     <footer className="flex justify-between items-center w-full relative  mb-[3dvh]">
-      <div className='flex gap-3 ml-[10px]'>
-        <img src={`images/global-gateway-euro.png`} alt="GlobalGateway" className="h-12" />
+      <div className='flex gap-3 ml-10 mb-[1.6dvh] '>
         <img src={`images/funded-by-ue.png`} alt="UE" className="h-12" />
+        <img src={`images/global-gateway-euro.png`} alt="GlobalGateway" className="h-12" />
+        <img src={`images/nstda-logo.png`} alt="NSTDA" className="h-12" />
       </div>
 
-      <div className='flex gap-[50px] mr-[50px]' >
+      <div className='flex gap-[50px] mr-[5dvw] mb-[1.5dvh]' >
         <img src={`images/IRD-logo.png`} alt="IRD" className="h-12" />
-        <img src={`images/nstda-logo.png`} alt="NSTDA" className="h-12" />
         <img src={`images/ctu-logo.png`} alt="CTU" className="h-12" />
       </div>
     </footer>
